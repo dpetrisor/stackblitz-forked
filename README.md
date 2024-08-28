eda314b3cc4c92f6f8b82dfbab288350304b9913
dpetrisor <dpetrisor@yahoo.com>
2024-08-28 14:58:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-forked
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dpetrisor/stackblitz-forked)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GridModule, PDFModule, ExcelModule } from '@progress/kendo-angular-grid';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        GridModule,
+        ChartsModule,
+        InputsModule,
+        PDFModule,
+        ExcelModule,
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
